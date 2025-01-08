48bea8ef11942cf4e4b7de60d9c3cccc53d3885c
unknown <pillorajem10@gmail.com>
2025-01-08 09:15:38 +0800

    modded

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 
 
 // Log all environment variables
+/*
 console.log('Environment Variables:', process.env.DB_HOST);
 console.log('Environment Variables:', process.env.DB_USER);
 console.log('Environment Variables:', process.env.DB_PASSWORD);
@@ -12,6 +13,7 @@ console.log('Environment Variables:', process.env.DB_NAME);
 console.log('Environment Variables:', process.env.PORT);
 console.log('Environment Variables:', process.env.JWT_SECRET);
 console.log('Environment Variables:', process.env.NODE_ENV);
+*/
 console.log('PROCESS ENV', process.env);  // Log all environment variables
 
 const http = require('http'); // For wrapping Express with Socket.IO
