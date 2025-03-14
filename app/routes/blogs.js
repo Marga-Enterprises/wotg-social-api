936368223a5c4d9926cee76030361772c66070bc
unknown <pillorajem10@gmail.com>
2025-03-14 09:48:57 +0800

    Replace mp4 format in upload video and made it webm

--- app/routes/blogs.js
+++ app/routes/blogs.js
@@ -6,6 +6,6 @@ const router = express.Router();
 // Blog Routes
 router.get("/", blogsController.list);
 router.get("/:id", blogsController.getById);
-router.post("/:id/upload-video", blogsController.uploadVideo); // Upload video for a specific blog
+router.put("/:id/upload-video", blogsController.uploadVideo); // Upload video for a specific blog
 
 module.exports = router;
